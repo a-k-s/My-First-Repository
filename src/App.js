af3f8cdc67355f904c9f3b3c1a158481f3c1b098
Anton Schuster <anton.k.schuster@gmail.com>
2021-10-18 16:08:40 -0500

    included year and school

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>Hello good people. </h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h2>Year: 2023, School: Latin School of Chicago</h2>
     </div>
   );
 }
